aabc77e47f8b48e60608d8fbba50ed39e335276d
Suhaani Agarwal <64849382+agarwalsuhaani@users.noreply.github.com>
2022-09-11 13:05:56 -0400

    Text Hello World added

--- app/src/main/java/edu/northeastern/numad22fa_suhaaniagarwal/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_suhaaniagarwal/MainActivity.java
@@ -2,7 +2,11 @@ package edu.northeastern.numad22fa_suhaaniagarwal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void toastMsg(String msg) {
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void displayToastMsg(View v) {
+        toastMsg("Suhaani Agarwal - agarwal.suh@northeastern.edu");
+    }
+
+    public void textMessage(View v) {
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText("Hello World!");
+        textView.setTextColor(Color.RED);
     }
 }
