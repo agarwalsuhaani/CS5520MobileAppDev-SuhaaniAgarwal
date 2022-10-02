f9e1576eaa7c10b9664c5f0e358037c2e83ad144
Suhaani Agarwal <64849382+agarwalsuhaani@users.noreply.github.com>
2022-10-02 02:08:11 -0400

    Dialog box added

---
+++ app/src/main/java/edu/northeastern/numad22fa_suhaaniagarwal/Link.java
@@ -0,0 +1,19 @@
+package edu.northeastern.numad22fa_suhaaniagarwal;
+
+public class Link {
+    private final String link;
+    private final String name;
+
+    public Link(String link, String name) {
+        this.link = link;
+        this.name = name;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
--- app/src/main/java/edu/northeastern/numad22fa_suhaaniagarwal/LinkCollector.java
+++ app/src/main/java/edu/northeastern/numad22fa_suhaaniagarwal/LinkCollector.java
@@ -1,14 +1,61 @@
 package edu.northeastern.numad22fa_suhaaniagarwal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LinkCollector extends AppCompatActivity {
+    RecyclerView linkRecyclerView;
+    FloatingActionButton fab;
+    String link;
+    String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_link_collector);
+
+        fab = findViewById(R.id.add_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Dialog dialog = new Dialog(LinkCollector.this);
+                dialog.setContentView(R.layout.dialog_box);
+                EditText edtLink = dialog.findViewById(R.id.editLink);
+                EditText edtName = dialog.findViewById(R.id.editName);
+                Button submit = dialog.findViewById(R.id.submit);
+
+                name = edtName.getText().toString();
+                link = edtLink.getText().toString();
+
+                submit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                });
+                //openDialog();
+                dialog.show();
+            }
+
+//            public void openDialog() {
+//                DialogBox dialogBox = new DialogBox();
+//                dialogBox.show(getSupportFragmentManager(), "Dialog Box");
+//            }
+        });
+
+
     }
 }
