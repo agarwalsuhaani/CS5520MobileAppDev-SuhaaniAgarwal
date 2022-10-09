4ecbcb521cb62dbcc3f4b73702347183c0626320
Suhaani Agarwal <64849382+agarwalsuhaani@users.noreply.github.com>
2022-10-09 16:39:53 -0400

    Find Prime, Runnable Thread works

--- app/src/main/java/edu/northeastern/numad22fa_suhaaniagarwal/Prime.java
+++ app/src/main/java/edu/northeastern/numad22fa_suhaaniagarwal/Prime.java
@@ -2,13 +2,67 @@ package edu.northeastern.numad22fa_suhaaniagarwal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Prime extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private Handler textHandler = new Handler();
+    private TextView txtStatus;
+    private Button terminate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prime);
+        txtStatus = findViewById(R.id.txtView_status);
+        terminate = findViewById(R.id.btn_terminate);
     }
+
+    public void runOnRunnableThread (View view) {
+        RunnableThread runnableThread = new RunnableThread();
+        new Thread(runnableThread).start();
+    }
+
+    class RunnableThread implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 3; i < 1000; i+=2) {
+                int count = 0;
+                for (int num = i; num>=1; num--) {
+                    if(i % num == 0) {
+                        count ++ ;
+                    }
+                }
+                textHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+
+                    }
+                });
+                if (count == 2 ) {
+                    int temp = i;
+                    textHandler.post(new Runnable() {
+                        @SuppressLint("SetTextI18n")
+                        @Override
+                        public void run() {
+                            txtStatus.setText("Current Prime Number: " + temp);
+                        }
+                    });
+                    Log.d(TAG, "Running on a different thread using Runnable Interface: " + i);
+                    try {
+                        Thread.sleep(1000); //Makes the thread sleep or be inactive for 10 seconds
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
 }
