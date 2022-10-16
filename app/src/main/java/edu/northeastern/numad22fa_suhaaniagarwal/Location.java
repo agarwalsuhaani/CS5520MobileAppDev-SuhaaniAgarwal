76d515dfb994da80e572977a6e103a4e1dfebf98
Suhaani Agarwal <64849382+agarwalsuhaani@users.noreply.github.com>
2022-10-16 19:34:38 -0400

    Current Location and Layout added

--- app/src/main/java/edu/northeastern/numad22fa_suhaaniagarwal/Location.java
+++ app/src/main/java/edu/northeastern/numad22fa_suhaaniagarwal/Location.java
@@ -1,14 +1,92 @@
 package edu.northeastern.numad22fa_suhaaniagarwal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Location extends AppCompatActivity {
+    private static final int REQUEST_LOCATION = 1;
+
+    Button getlocationBtn, btn_reset;
+    TextView showLocationTxt, distanceTxt;
+
+    LocationManager locationManager;
+    String latitude, longitude;
+
+    double distance = 0.0;
+
+    android.location.Location mPreviousLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+
+        ActivityCompat.requestPermissions(this, new String[]
+                {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+
+        mPreviousLocation = new android.location.Location("A");
+        mPreviousLocation.getLatitude();
+        mPreviousLocation.getLongitude();
+
+        showLocationTxt = findViewById(R.id.show_location);
+        getlocationBtn = findViewById(R.id.getLocation);
+        distanceTxt = findViewById(R.id.txt_distance);
+        btn_reset = findViewById(R.id.btn_reset);
     }
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(Location.this,Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(Location.this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) !=PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[]
+                    {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        }
+        else {
+            android.location.Location LocationGps= locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            android.location.Location LocationNetwork=locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            android.location.Location LocationPassive=locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+
+            if (LocationGps !=null) {
+                double lat=LocationGps.getLatitude();
+                double longi=LocationGps.getLongitude();
+
+                latitude=String.valueOf(lat);
+                longitude=String.valueOf(longi);
+
+                showLocationTxt.setText("Your Location:"+"\n"+"Latitude= "+latitude+"\n"+"Longitude= "+longitude);
+            }
+            else if (LocationNetwork !=null) {
+                double lat=LocationNetwork.getLatitude();
+                double longi=LocationNetwork.getLongitude();
+
+                latitude=String.valueOf(lat);
+                longitude=String.valueOf(longi);
+
+                showLocationTxt.setText("Your Location:"+"\n"+"Latitude= "+latitude+"\n"+"Longitude= "+longitude);
+
+            }
+            else if (LocationPassive !=null) {
+                double lat=LocationPassive.getLatitude();
+                double longi=LocationPassive.getLongitude();
+
+                latitude=String.valueOf(lat);
+                longitude=String.valueOf(longi);
+
+                showLocationTxt.setText("Your Location:"+"\n"+"Latitude= "+latitude+"\n"+"Longitude= "+longitude);
+            }
+            else {
+                Toast.makeText(this, "Can't Get Your Location", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
 }
